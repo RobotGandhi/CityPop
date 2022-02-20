01df75e003a2cc14990d3ff4e0dbd30d679a8844
Viktor Carlsson <99viktor.vc@gmail.com>
2022-02-20 14:10:14 +0100

    Multiple screens and navigations

--- App.js
+++ App.js
@@ -1,20 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, Button, StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+        />
+        <Stack.Screen
+          name="City Search"
+          component={CitySearchScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+
+const HomeScreen = ({navigation}) => {
+  return (
+    <View style={styles.container}>
+      <Text>Hello, World!</Text>
+      <Button
+        title="City Search"
+        onPress={() => navigation.navigate("City Search")}
+      />
+      <Button 
+        title="Tja2"
+        onPress={() => console.log("Tja2")}
+     />
+    </View>
+  );
+};
+
+const CitySearchScreen = ({navigation, route}) => {
+  return (
+    <View style={styles.container}>
+      <Text>Välkommen till sökskärmen</Text>
+      <Button
+        title="Tja"
+        onPress={() => console.log("Tja")}
+      />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
   },
 });
